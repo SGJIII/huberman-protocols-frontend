d4c42b72562b4c94f339d16f89d3fd0ef61f08ea
Sam Johnson <samueljoh@gmail.com>
2024-06-02 16:41:19 -0700

    Update the next.config.mjs to include the src directory as the base directory for the Next.js app.

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,13 @@ const nextConfig = {
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   },
+  webpack: (config) => {
+    config.resolve.modules.push(__dirname + '/src');
+    return config;
+  },
 };
 
 export default nextConfig;
+
 
   
