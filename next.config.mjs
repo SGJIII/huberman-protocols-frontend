3c691f274db3ea4d4020064c0d8174512fcca576
Sam Johnson <samueljoh@gmail.com>
2024-06-02 16:28:25 -0700

    made edits to config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
-// frontend/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-      AUTH0_SECRET: process.env.AUTH0_SECRET,
-      AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
-      AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  },
+};
+
+export default nextConfig;
+
   
