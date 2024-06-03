2b10398ea577fc3dcde82c2644396a6cd7210285
Sam Johnson <samueljoh@gmail.com>
2024-06-02 19:27:17 -0700

    fixed capitalization issue again

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 // src/pages/login.js
 import dynamic from 'next/dynamic';
 
-const Login = dynamic(() => import('../components/login'), { ssr: false });
+const Login = dynamic(() => import('../components/Login'), { ssr: false });
 
 export default Login;
